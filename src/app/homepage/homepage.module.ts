f1d9b0e3f8d1ea5c287ce7b1b89e784ce81d48a7
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-12-04 14:05:45 -0300

    Criação da homepage

---
+++ src/app/homepage/homepage.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomepageRoutingModule } from './homepage-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+@NgModule({
+  declarations: [
+    HomepageComponent
+  ],
+  imports: [
+    CommonModule,
+    HomepageRoutingModule
+  ]
+})
+export class HomepageModule { }

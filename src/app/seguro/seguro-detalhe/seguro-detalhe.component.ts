1b1d9895990a7561e895becca56e2110e601984c
fernanda da lapa <fernandadalapa@hotmail.com>
2023-12-05 11:25:58 -0300

    Implementação de seguro detalhe

--- src/app/seguro/seguro-detalhe/seguro-detalhe.component.ts
+++ src/app/seguro/seguro-detalhe/seguro-detalhe.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Seguro } from 'src/app/shared/model/seguro';
+import { SeguroService } from 'src/app/shared/service/seguro.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-seguro-detalhe',
   templateUrl: './seguro-detalhe.component.html',
   styleUrls: ['./seguro-detalhe.component.scss']
 })
-export class SeguroDetalheComponent {
+export class SeguroDetalheComponent implements OnInit{
+  seguro:Seguro = new Seguro();
+  public idSeguro: number
 
+  @ViewChild('ngForm')
+  public ngForm: NgForm;
+
+  constructor(private seguroService : SeguroService,
+              private router: Router,
+              private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idSeguro = params['id'];
+
+      if(this.idSeguro){
+        this.buscarSeguro();
+      }
+    });
+
+  }
+  voltar(){
+    this.router.navigate(['/seguros/lista'])
+  }
+
+  buscarSeguro() {
+    this.seguroService.pesquisarPorId(this.idSeguro).subscribe(
+      resultado =>{
+        this.seguro = resultado;
+      },
+      erro =>{
+        Swal.fire('Erro', 'Erro ao buscar seguro com ID (' + this.idSeguro + ') : ', 'error');
+        return;
+      }
+    )
+  }
+
+salvar(form: NgForm){
+  if(form.invalid){
+    Swal.fire("Erro", "Formulário inválido", 'error');
+  }
+
+  if(this.idSeguro){
+    this.atualizar();
+  } else {
+    this.inserirSeguro();
+  }
+}
+inserirSeguro() {
+  this.seguroService.salvar(this.seguro).subscribe(
+    sucesso => {
+      Swal.fire("Sucesso", "Seguro salvo com sucesso", 'success');
+      this.seguro = new Seguro();
+    },
+    erro => {
+      Swal.fire("Erro", "Não foi possivel salvar o seguro: " + erro, 'error');
+    }
+  )
+}
+atualizar() {
+  this.seguroService.atualizar(this.seguro).subscribe(
+    sucesso => {
+      Swal.fire("Sucesso", "Seguro Atualizado com Sucesso!", 'success');
+    },
+    erro => {
+      Swal.fire("Erro", "Não foi possivel atualizar o seguro", 'error');
+    }
+  )
+}
 }
